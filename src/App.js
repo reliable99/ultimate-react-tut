3d9e3e7ba53f104f13f6cc3cb7eb4a8f574f35ca
reliable99 <abdulmuhminsodiq@gmail.com>
2023-11-01 00:23:06 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./index.css";
 import Header from "./Header";
 import Content from "./Content";
 import Footer from "./Footer";
--- src/Content.js
+++ src/Content.js
@@ -6,9 +6,22 @@ const Content = () => {
     const int = Math.floor(Math.random() * 3);
     return names[int];
   };
+
+  const handleClick = () => {
+    console.log("You clicked it");
+  };
+  const handleClick2 = (name) => {
+    console.log(`${name} was clicked`);
+  };
+  const handleClick3 = (e) => {
+    console.log(e.target.innerText);
+  };
   return (
     <main>
-      <p>hello {handleNameChnage()}</p>
+      <p onDoubleClick={handleClick}>hello {handleNameChnage()}</p>
+      <button onClick={handleClick}>click it</button>
+      <button onClick={() => handleClick2("Dele")}>click it</button>
+      <button onClick={(e) => handleClick3(e)}>click it</button>
     </main>
   );
 };
