1b876a6e89492e56a359d38099f9be9fe730f213
reliable99 <abdulmuhminsodiq@gmail.com>
2023-11-01 09:04:57 +0100

    second commit

--- src/Content.js
+++ src/Content.js
@@ -1,27 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaTrashAlt } from "react-icons/fa";
 
 const Content = () => {
-  const handleNameChnage = () => {
-    const names = ["bobs", "kelvin", "dave"];
-    const int = Math.floor(Math.random() * 3);
-    return names[int];
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      checked: true,
+      item: "One half pound bag of Cocoa Covered Almonds Unsalted",
+    },
+    {
+      id: 2,
+      checked: false,
+      item: "Item 2",
+    },
+    {
+      id: 3,
+      checked: false,
+      item: "Item 3",
+    },
+  ]);
+
+  const handleCheck = (id) => {
+    const listItems = items.map((item) =>
+      item.id === id ? { ...item, checked: !item.checked } : item
+    );
+    setItems(listItems);
   };
 
-  const handleClick = () => {
-    console.log("You clicked it");
-  };
-  const handleClick2 = (name) => {
-    console.log(`${name} was clicked`);
-  };
-  const handleClick3 = (e) => {
-    console.log(e.target.innerText);
-  };
   return (
     <main>
-      <p onDoubleClick={handleClick}>hello {handleNameChnage()}</p>
-      <button onClick={handleClick}>click it</button>
-      <button onClick={() => handleClick2("Dele")}>click it</button>
-      <button onClick={(e) => handleClick3(e)}>click it</button>
+      <ul>
+        {items.map((item) => (
+          <li className="item" key={item.id}>
+            <input
+              type="checkbox"
+              onChange={() => handleCheck(item.id)}
+              checked={item.checked}
+            />
+            <label>{item.item}</label>
+            <FaTrashAlt role="button" tabIndex="0" />
+          </li>
+        ))}
+      </ul>
     </main>
   );
 };
